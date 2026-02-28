0738599d9d82a15ef1488c666c1215dd65050401
MDZAHIDHASAN2024 <zahidbinanwar3@gmail.com>
2026-02-28 23:47:46 +0600

    fix cors duplicate

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const app = express();
-const cors = require('cors');
+
 app.use(
   cors({
     origin: ['http://localhost:3000', 'https://your-app.vercel.app'],
